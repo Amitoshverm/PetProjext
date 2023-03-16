d4ea7784b2cc1aae88179fe689a86053168563cb
Amitosh <amitosh.verma9@gmail.com>
2023-03-17 00:35:25 +0530

    Entities added
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/com/blogapis/bloggingappllication/Entity/UserEntity.java
+++ src/main/java/com/blogapis/bloggingappllication/Entity/UserEntity.java
@@ -1,2 +1,26 @@
-package com.blogapis.bloggingappllication.Entity;public class UserEntity {
+package com.blogapis.bloggingappllication.Entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Integer userId;
+    @Column(name= "user_name", nullable = false)
+    String username;
+    @Column(name = "Email", nullable = false)
+    String email;
+    String password;
+    String about;
+
 }
