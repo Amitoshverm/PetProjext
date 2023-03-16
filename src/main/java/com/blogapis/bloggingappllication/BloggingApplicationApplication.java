ad311eed702f6b17f660dfd89d5ab563e40d52a5
Amitosh <amitosh.verma9@gmail.com>
2023-03-16 23:34:26 +0530

    initialised project
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

---
+++ src/main/java/com/blogapis/bloggingappllication/BloggingApplicationApplication.java
@@ -0,0 +1,15 @@
+package com.blogapis.bloggingappllication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingApplicationApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BloggingApplicationApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/blogapis/bloggingappllication/BloggingApplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogapis.bloggingappllication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingApplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
