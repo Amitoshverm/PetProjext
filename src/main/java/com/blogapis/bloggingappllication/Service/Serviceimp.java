d2d88013d51cf5fece36631a1508d2110e8e875e
Amitosh <amitosh.verma9@gmail.com>
2023-03-19 13:05:04 +0530

    comments for understanding
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/com/blogapis/bloggingappllication/Service/Serviceimp.java
+++ src/main/java/com/blogapis/bloggingappllication/Service/Serviceimp.java
@@ -6,6 +6,11 @@ import java.util.List;
 
 public interface Serviceimp {
 
+    // This class is just basic operation for service this could be done directly without
+    // creating this Serviceimp, just write these methods directly to UserService class without this imp class
+
+
+
     UserDTO createUser(UserDTO userDTO);
     List<UserDTO> getAllUsers();
     UserDTO updateUser(UserDTO userDTO, Integer userId);
