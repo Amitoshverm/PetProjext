38cdf42bfacd7d176a58c86d31b039e15f091943
Amitosh <amitosh.verma9@gmail.com>
2023-03-19 13:04:18 +0530

    added date attribute
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/com/blogapis/bloggingappllication/DTO/ApiResponse.java
+++ src/main/java/com/blogapis/bloggingappllication/DTO/ApiResponse.java
@@ -1,2 +1,20 @@
-package com.blogapis.bloggingappllication.DTO;public class ApiResponse {
+package com.blogapis.bloggingappllication.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse {
+
+    private Date date;
+    private String message;
+    private boolean success;
+
 }
