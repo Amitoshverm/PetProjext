22831e6f0c4a26d2c7738fa12cc81ce7fb8806e5
Amitosh <amitosh.verma9@gmail.com>
2023-03-20 20:18:46 +0530

    Method Argument not Valid Exception added
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/com/blogapis/bloggingappllication/CustomException/GlobalExceptionHandler.java
+++ src/main/java/com/blogapis/bloggingappllication/CustomException/GlobalExceptionHandler.java
@@ -3,18 +3,36 @@ package com.blogapis.bloggingappllication.CustomException;
  import com.blogapis.bloggingappllication.DTO.ApiResponse;
  import org.springframework.http.HttpStatus;
  import org.springframework.http.ResponseEntity;
+ import org.springframework.validation.FieldError;
+ import org.springframework.web.bind.MethodArgumentNotValidException;
  import org.springframework.web.bind.annotation.ExceptionHandler;
  import org.springframework.web.bind.annotation.RestControllerAdvice;
 
  import java.util.Date;
+ import java.util.HashMap;
+ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ResourceNotFoundException.class)
+    @ExceptionHandler(ResourceNotFoundException.class)// This exception will arise if the provided id is not found(while updating, finding or delete, inside service) or incorrect and here date will tell that when was id given just optional
     public ResponseEntity<ApiResponse> responseNotFoundExceptionHandler(ResourceNotFoundException ex){
         String message = ex.getMessage();
         ApiResponse apiResponse = new ApiResponse(new Date(), message, false);
         return new ResponseEntity<ApiResponse>(apiResponse, HttpStatus.NOT_FOUND);
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class) // This method will give exception, if the inputs provided by user to server are not valid
+    public ResponseEntity<Map<String, String>> handleMethodArgsNotValidException(MethodArgumentNotValidException ex){
+        Map<String, String> response = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error)->{
+            //typecast to FieldError to getField()
+            String fieldName = ((FieldError)error).getField();
+            String message = error.getDefaultMessage();
+            response.put(fieldName, message);
+        });
+
+        return new ResponseEntity<Map<String,String>>(response, HttpStatus.BAD_REQUEST);
     }
 }
