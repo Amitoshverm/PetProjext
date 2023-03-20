7b3cbb1c61c28479f4a66afeb743b08fc16ae003
Amitosh <amitosh.verma9@gmail.com>
2023-03-20 20:21:41 +0530

    Validation annotations like @NotEmpty, @Email...etc
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/com/blogapis/bloggingappllication/DTO/UserDTO.java
+++ src/main/java/com/blogapis/bloggingappllication/DTO/UserDTO.java
@@ -4,14 +4,26 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.*;
+
 @NoArgsConstructor
 @Getter
 @Setter
 public class UserDTO {
 
     Integer userId;
+
+    @NotEmpty
+    @Size(min = 2, message = "name must be more than 2 character")
     String username;
+
+    @Email(message = "Email address not valid !!")
     String email;
+
+    @NotNull
+    @Pattern(regexp = "^([a-zA-Z0-9]{8})", message = "Password should contain one UpperCase, number and should have length of 8")
     String password;
+
+    @NotNull
     String about;
 }
