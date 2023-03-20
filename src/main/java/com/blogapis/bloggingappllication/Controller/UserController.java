fd951b3a96d75e78513f89166fa9c8beebae069e
Amitosh <amitosh.verma9@gmail.com>
2023-03-20 20:16:19 +0530

    Enabled Validation from controller using @Valid annotation
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/com/blogapis/bloggingappllication/Controller/UserController.java
+++ src/main/java/com/blogapis/bloggingappllication/Controller/UserController.java
@@ -11,12 +11,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.annotation.RequestScope;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
+@SuppressWarnings("unchecked")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -24,7 +25,7 @@ public class UserController {
     UserServiceimp userService;
 
     @PostMapping("/")
-    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO){
+    public ResponseEntity<UserDTO> createUser(@Valid @RequestBody UserDTO userDTO){ // Valid is used to enable annotations on the dto bean
          UserDTO userCreated = this.userService.createUser(userDTO);
          return new ResponseEntity<>(userCreated, HttpStatus.CREATED);
     }
@@ -41,7 +42,7 @@ public class UserController {
     }
 
     @PutMapping("/{userId}")
-    public ResponseEntity<UserDTO> updateUser(@RequestBody UserDTO userDTO, @PathVariable Integer userId){
+    public ResponseEntity<UserDTO> updateUser(@Valid @RequestBody UserDTO userDTO, @PathVariable Integer userId){ // Valid is used to enable annotations on the dto bean
         UserDTO updatedUser = this.userService.updateUser(userDTO, userId);
         return  ResponseEntity.ok(updatedUser);
     }
