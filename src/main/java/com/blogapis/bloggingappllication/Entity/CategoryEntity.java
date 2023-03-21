7a73cf1042dcac38e68b20d792cf65adf4dfa573
Amitosh <amitosh.verma9@gmail.com>
2023-03-21 15:19:59 +0530

    Fields of category entity added
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/com/blogapis/bloggingappllication/Entity/CategoryEntity.java
+++ src/main/java/com/blogapis/bloggingappllication/Entity/CategoryEntity.java
@@ -1,2 +1,25 @@
-package com.blogapis.bloggingappllication.Entity;public class CategoryEntity {
+package com.blogapis.bloggingappllication.Entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "category")
+public class CategoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer categoryId;
+
+    @Column(name = "title")
+    private String categoryTitle;
+
+    @Column(name = "description")
+    private String categoryDescription;
+
 }
