4fdcd8a033e9dbcee01fb1b6443e4de39806f70c
Amitosh <amitosh.verma9@gmail.com>
2023-03-21 21:27:51 +0530

    Apis created
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/com/blogapis/bloggingappllication/Controller/CategoryController.java
+++ src/main/java/com/blogapis/bloggingappllication/Controller/CategoryController.java
@@ -1,2 +1,56 @@
-package com.blogapis.bloggingappllication.Controller;public class CategoryController {
+package com.blogapis.bloggingappllication.Controller;
+
+import com.blogapis.bloggingappllication.DTO.ApiResponse;
+import com.blogapis.bloggingappllication.DTO.CategoryDTO;
+import com.blogapis.bloggingappllication.Entity.CategoryEntity;
+import com.blogapis.bloggingappllication.Service.CategoryService.CategoryServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryServiceImp categoryService;
+
+    //CREATE
+    @PostMapping("/")
+    public ResponseEntity<CategoryDTO> createCategory(@Valid @RequestBody CategoryDTO categoryDTO){
+        CategoryDTO categoryDTO1 = this.categoryService.createCategory(categoryDTO);
+        return new ResponseEntity<>(categoryDTO1, HttpStatus.CREATED);
+    }
+
+    //UPDATE
+    @PutMapping("/{categoryId}")
+    public ResponseEntity<CategoryDTO> updateCategory(@Valid @RequestBody CategoryDTO categoryDTO, @PathVariable Integer categoryId){
+        CategoryDTO categoryDTO1 = this.categoryService.updateCategory(categoryDTO, categoryId);
+        return ResponseEntity.ok(categoryDTO1);
+    }
+
+    //DELETE
+    @DeleteMapping("/{categoryId}")
+    public ResponseEntity<ApiResponse> deleteCategory(@PathVariable Integer categoryId){
+        this.categoryService.deleteCategory(categoryId);
+        return new ResponseEntity<>(new ApiResponse(new Date(), "Category deleted successfully", true),HttpStatus.OK);
+    }
+    //GET
+    @GetMapping("/{categoryId}")
+    public ResponseEntity<CategoryDTO> getById(@PathVariable Integer categoryId){
+        CategoryDTO categorydto= this.categoryService.getCategoryById(categoryId);
+        return ResponseEntity.ok(categorydto);
+    }
+    //GET ALL
+    @GetMapping("/")
+    public ResponseEntity<List<CategoryDTO>> getAllCategory(){
+        List<CategoryDTO> categoryDTOS = this.categoryService.getAllCategory();
+        return ResponseEntity.ok(categoryDTOS);
+    }
+
 }
