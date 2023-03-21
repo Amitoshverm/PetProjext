93f6726e969d85c605536249c0f2d4cb0f091f1c
Amitosh <amitosh.verma9@gmail.com>
2023-03-21 15:19:15 +0530

    added DTO of Category Entity
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/com/blogapis/bloggingappllication/DTO/CategoryDTO.java
+++ src/main/java/com/blogapis/bloggingappllication/DTO/CategoryDTO.java
@@ -1,2 +1,14 @@
-package com.blogapis.bloggingappllication.DTO;public class CategoryDTO {
+package com.blogapis.bloggingappllication.DTO;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CategoryDTO {
+    private Integer categoryId;
+    private String categoryTitle;
+    private String categoryDescription;
 }
