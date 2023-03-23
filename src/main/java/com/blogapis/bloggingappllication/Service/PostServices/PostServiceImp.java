1f2f69646d01f358f608bba9082cc6c9ad2c3f40
Amitosh <amitosh.verma9@gmail.com>
2023-03-23 22:50:33 +0530

    created Methods of PostService
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/com/blogapis/bloggingappllication/Service/PostServices/PostServiceImp.java
+++ src/main/java/com/blogapis/bloggingappllication/Service/PostServices/PostServiceImp.java
@@ -1,2 +1,99 @@
-package com.blogapis.bloggingappllication.Service.PostServices;public class PostServiceImp {
+package com.blogapis.bloggingappllication.Service.PostServices;
+
+import com.blogapis.bloggingappllication.CustomException.ResourceNotFoundException;
+import com.blogapis.bloggingappllication.DTO.PostDTO;
+import com.blogapis.bloggingappllication.DTO.UserDTO;
+import com.blogapis.bloggingappllication.Entity.CategoryEntity;
+import com.blogapis.bloggingappllication.Entity.PostEntity;
+import com.blogapis.bloggingappllication.Entity.UserEntity;
+import com.blogapis.bloggingappllication.Repository.CategoryRepository;
+import com.blogapis.bloggingappllication.Repository.PostRepository;
+import com.blogapis.bloggingappllication.Repository.UserRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PostServiceImp implements PostServiceMeths{
+
+    @Autowired
+    private PostRepository postRepository;
+    @Autowired
+    private ModelMapper modelMapper;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Override
+    public PostDTO createPost(PostDTO postDTO, Integer userId, Integer categoryId) {
+        UserEntity user = this.userRepository.findById(userId)
+                .orElseThrow(()-> new ResourceNotFoundException("User", "userId", userId));
+
+        CategoryEntity category = this.categoryRepository.findById(categoryId)
+                .orElseThrow(()-> new ResourceNotFoundException("category", "categoryId", categoryId));
+
+        PostEntity postEntity1 = this.modelMapper.map(postDTO, PostEntity.class);
+        postEntity1.setImage("default.png");
+        postEntity1.setAddDate(new Date());
+        postEntity1.setUser(user);
+        postEntity1.setCategory(category);
+
+        PostEntity savedPost = this.postRepository.save(postEntity1);
+        return this.modelMapper.map(savedPost, PostDTO.class);
+    }
+
+    @Override
+    public PostEntity updatePost(PostEntity post, Integer postId) {
+        return null;
+    }
+
+    @Override
+    public void deletePost(Integer postId) {
+
+    }
+
+    @Override
+    public PostEntity getPostById(Integer postId) {
+        return null;
+    }
+
+    @Override
+    public List<PostEntity> getAllPosts() {
+        return null;
+    }
+
+    @Override
+    public List<PostDTO> getPostsByCategory(Integer categoryId) {
+        CategoryEntity category = this.categoryRepository.findById(categoryId)
+                .orElseThrow(()->new ResourceNotFoundException("category", "categoryId", categoryId));
+        List<PostEntity> postEntities = this.postRepository.findByCategory(category);
+
+        return postEntities
+                .stream()
+                .map((PostEntity)-> this.modelMapper.map(postEntities, PostDTO.class))
+                .collect(Collectors.toList());
+
+    }
+
+    @Override
+    public List<PostDTO> getPostsByUser(Integer userId) {
+        UserEntity user = this.userRepository.findById(userId).orElseThrow(()-> new ResourceNotFoundException("user", "userId", userId));
+        List<PostEntity> postEntities = this.postRepository.findByUser(user);
+        List<PostDTO> posts = postEntities
+                .stream()
+                .map((PostEntity)-> this.modelMapper.map(postEntities, PostDTO.class))
+                .collect(Collectors.toList());
+
+        return posts;
+    }
+
+    @Override
+    public List<PostEntity> searchPosts(String keyword) {
+        return null;
+    }
 }
--- src/main/java/com/blogapis/bloggingappllication/Service/PostServices/PostServiceMeths.java
+++ src/main/java/com/blogapis/bloggingappllication/Service/PostServices/PostServiceMeths.java
@@ -1,2 +1,34 @@
-package com.blogapis.bloggingappllication.Service.PostServices;public interface PostServiceMeths {
+package com.blogapis.bloggingappllication.Service.PostServices;
+
+import com.blogapis.bloggingappllication.DTO.PostDTO;
+import com.blogapis.bloggingappllication.Entity.PostEntity;
+
+import java.util.List;
+
+public interface PostServiceMeths {
+
+    // CREATE
+    PostDTO createPost(PostDTO postDTO, Integer categoryId, Integer userId);
+
+    //UPDATE
+    PostEntity updatePost(PostEntity post, Integer postId);
+
+    //DELETE
+    void deletePost(Integer postId);
+
+    //GET
+    PostEntity getPostById(Integer postId);
+
+    //GET ALL
+    List<PostEntity> getAllPosts();
+
+    //GET ALL POST BY CATEGORY
+    List<PostDTO> getPostsByCategory(Integer categoryId);
+
+    //GET ALL POST BY USER
+    List<PostDTO> getPostsByUser(Integer userId);
+
+    //SEARCH POSTS
+    List<PostEntity> searchPosts(String keyword);
+
 }
