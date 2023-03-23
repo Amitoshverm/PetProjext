35926308e7f613aa59cce8b06cdeba94c129b3db
Amitosh <amitosh.verma9@gmail.com>
2023-03-23 22:50:55 +0530

    created Repo fot posts
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/com/blogapis/bloggingappllication/Repository/PostRepository.java
+++ src/main/java/com/blogapis/bloggingappllication/Repository/PostRepository.java
@@ -1,2 +1,14 @@
-package com.blogapis.bloggingappllication.Repository;public interface PostRepository {
+package com.blogapis.bloggingappllication.Repository;
+
+import com.blogapis.bloggingappllication.Entity.CategoryEntity;
+import com.blogapis.bloggingappllication.Entity.PostEntity;
+import com.blogapis.bloggingappllication.Entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<PostEntity, Integer> {
+
+    List<PostEntity> findByUser(UserEntity user);
+    List<PostEntity> findByCategory(CategoryEntity category);
 }
