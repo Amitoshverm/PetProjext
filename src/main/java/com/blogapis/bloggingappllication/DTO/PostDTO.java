7cf75d0b1b07a12a8ecfb33e122a6898a4efdf98
Amitosh <amitosh.verma9@gmail.com>
2023-03-23 22:49:46 +0530

    created Posts Dto
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/com/blogapis/bloggingappllication/DTO/PostDTO.java
+++ src/main/java/com/blogapis/bloggingappllication/DTO/PostDTO.java
@@ -1,2 +1,26 @@
-package com.blogapis.bloggingappllication.DTO;public class PostDTO {
+package com.blogapis.bloggingappllication.DTO;
+
+import com.blogapis.bloggingappllication.Entity.CategoryEntity;
+import com.blogapis.bloggingappllication.Entity.UserEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PostDTO {
+
+    private Integer postId;
+
+    private String postTitle;
+    private String content;
+    private String image;
+    private Date date;
+
+    private CategoryDTO category; // if we have used entities then it will have gone to infinite loop
+    private UserDTO user; // vice versa
+
 }
