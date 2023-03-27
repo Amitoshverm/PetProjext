43b007be4f17a082ecb16d34746c2fbf3773836a
Amitosh <amitosh.verma9@gmail.com>
2023-03-28 00:20:57 +0530

    PostResponse for paginaiton
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

---
+++ src/main/java/com/blogapis/bloggingappllication/Payload/PostResponse.java
@@ -0,0 +1,23 @@
+package com.blogapis.bloggingappllication.Payload;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PostResponse {
+
+    private List<PostDTO> content;
+    private Integer pageNumber;
+    private Integer pageSize;
+    private Long totalElement;
+    private Integer totalPages;
+    private Boolean lastPage;
+
+
+}
