629f40a3a92a0b3f247ce1511b18350e96d5d51a
Amitosh <amitosh.verma9@gmail.com>
2023-04-01 23:38:29 +0530

    this class will load the user detail after client is authenticated
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/com/blogapis/bloggingappllication/CustomUserDetailService.java
+++ src/main/java/com/blogapis/bloggingappllication/CustomUserDetailService.java
@@ -1,2 +1,27 @@
-package com.blogapis.bloggingappllication;public class CustomUserDetailService {
+package com.blogapis.bloggingappllication;
+
+import com.blogapis.bloggingappllication.CustomException.ForStringResourceNotFound;
+import com.blogapis.bloggingappllication.CustomException.ResourceNotFoundException;
+import com.blogapis.bloggingappllication.Entity.UserEntity;
+import com.blogapis.bloggingappllication.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        //Load user from database by username
+        UserEntity user =  this.userRepository.findByEmail(username)
+                .orElseThrow(()-> new ResourceNotFoundException("user", "email "+ username, 0));
+
+        return user;
+    }
 }
