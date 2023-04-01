c1cff221d0563ffe81d6b84334020b33cdfd9d06
Amitosh <amitosh.verma9@gmail.com>
2023-04-01 23:45:45 +0530

    Securing our data in DB so that which are authenticated will only request data
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/com/blogapis/bloggingappllication/CustomException/ForStringResourceNotFound.java
+++ src/main/java/com/blogapis/bloggingappllication/CustomException/ForStringResourceNotFound.java
@@ -1,2 +1,16 @@
-package com.blogapis.bloggingappllication.CustomException;public class ForStringResourceNotFound {
+package com.blogapis.bloggingappllication.CustomException;
+
+public class ForStringResourceNotFound extends RuntimeException {
+
+    String resourceName;
+    String fieldName;
+    String param;
+
+    public ForStringResourceNotFound(String resourceName, String fieldName, String param) {
+        super(String.format("%s not found with %s : %s", resourceName, fieldName, param));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.param = param;
+
+    }
 }
--- src/main/java/com/blogapis/bloggingappllication/Repository/UserRepository.java
+++ src/main/java/com/blogapis/bloggingappllication/Repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.blogapis.bloggingappllication.Repository;
 import com.blogapis.bloggingappllication.Entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    Optional<UserEntity> findByEmail(String email);
+
 }
