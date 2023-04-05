bd28ce62338283053d988725033ca4bef6c18d1a
Amitosh <amitosh.verma9@gmail.com>
2023-04-05 17:16:44 +0530

    Controller class for Authentication
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

--- src/main/java/com/blogapis/bloggingappllication/Controller/AuthController.java
+++ src/main/java/com/blogapis/bloggingappllication/Controller/AuthController.java
@@ -1,2 +1,47 @@
-package com.blogapis.bloggingappllication.Controller;public class AuthController {
+package com.blogapis.bloggingappllication.Controller;
+
+import com.blogapis.bloggingappllication.Payload.JwtAuthRequest;
+import com.blogapis.bloggingappllication.Payload.JwtAuthResponse;
+import com.blogapis.bloggingappllication.Security.JWTTokenHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth/")
+public class AuthController {
+
+
+    @Autowired
+    private JWTTokenHelper jwtTokenHelper;
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtAuthResponse> createToken(@RequestBody JwtAuthRequest request) {
+
+        this.authenticate(request.getUsername(), request.getPassword());
+        UserDetails userDetails = this.userDetailsService.loadUserByUsername(request.getUsername());
+        String token = this.jwtTokenHelper.generateToken(userDetails);
+        JwtAuthResponse response = new JwtAuthResponse();
+        response.setToken(token);
+        return new ResponseEntity<JwtAuthResponse>(response, HttpStatus.OK);
+    }
+    private void authenticate(String username, String password){
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+
+            this.authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+
+    }
 }
