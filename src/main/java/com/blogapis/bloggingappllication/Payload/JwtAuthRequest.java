1fce8f73f0f9df325f8c1e1c8f0e8939210326d6
Amitosh <amitosh.verma9@gmail.com>
2023-04-05 17:08:10 +0530

    added JWT dependency
    Signed-off-by: Amitosh <amitosh.verma9@gmail.com>

---
+++ src/main/java/com/blogapis/bloggingappllication/Payload/JwtAuthRequest.java
@@ -0,0 +1,2 @@
+package com.blogapis.bloggingappllication.Payload;public class JwtAuthRequeset {
+}
---
+++ src/main/java/com/blogapis/bloggingappllication/Security/JwtAuthenticationFilter.java
@@ -0,0 +1,2 @@
+package com.blogapis.bloggingappllication.Security;public class JwtAuthenticationFilter {
+}
